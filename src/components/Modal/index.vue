ea7200ab054093730cb5b08f1c1b4bde9bea6855
Konami Ota <konami.backham@gmail.com>
2021-12-28 18:28:36 +0900

    add modal

---
+++ src/components/Modal/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="overlay">
+    <div id="content">
+      <img :src="val.image" />
+      <p>説明：{{ val.image }}</p>
+
+      <button @click="$emit('close')">閉じる</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["val"],
+};
+</script>
+
+<style>
+#overlay {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#content {
+  z-index: 2;
+  width: 50%;
+  padding-top: 5em;
+  padding-bottom: 5em;
+  text-align: center;
+}
+
+#content > img {
+  width: 95%;
+}
+</style>
