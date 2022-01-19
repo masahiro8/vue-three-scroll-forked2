2be73392150ce9a913846c3560ba712242f089f4
masahiro8 <masahiro819@gmail.com>
2022-01-19 11:09:16 +0900

    gltfからアニメーションを取得してスクロール再生を実装

---
+++ src/components/ThreeScene/components/scene_animate.js
@@ -0,0 +1,187 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// eslint-disable-next-line
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { rotationToRadian } from "../../../util/rotationToRadian";
+
+export const Scene = () => {
+  let scene, mainCamera, renderer, controls, animationMixer;
+  let duration = 10;
+  let moveProgress = 0;
+
+  // Orbit設定
+  const orbitConfig = {
+    enableDamping: true,
+    dampingFactor: 0.25,
+    enableZoom: true,
+  };
+
+  /**
+   * @description シーンの初期化
+   * @param {int,int,int}
+   */
+  const init = async ({ modelUrl, canvasId, width, height }) => {
+    //シーン
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    renderer.gammaOutput = true;
+    renderer.setClearColor(0xffff00, 1);
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(
+      window.devicePixelRatio ? window.devicePixelRatio : 1
+    );
+    document.getElementById(canvasId).appendChild(renderer.domElement);
+
+    //ライト
+    const light = new THREE.AmbientLight(0xffffff, 1.0);
+    scene.background = new THREE.Color(0x202020);
+    scene.add(light);
+
+    //ヘルパー
+    const axis = new THREE.AxesHelper(200);
+    scene.add(axis);
+    axis.position.set(0, 0, 0);
+
+    // データロード
+    const { model, animations, camera, mixer } = await loadGLTF(modelUrl);
+    console.log("load", camera, animations);
+
+    // モデルをセット
+    if (model) {
+      model.position.set(0, -1, 0);
+      scene.add(model);
+    }
+
+    //カメラ無ければセット
+    if (!camera) {
+      mainCamera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        500
+      );
+      mainCamera.position.set(1, 1, 1);
+      mainCamera.lookAt(new THREE.Vector3(0, 0, 0));
+
+      // Orbit
+      controls = new OrbitControls(mainCamera, renderer.domElement);
+      controls.enableDamping = orbitConfig.enableDamping;
+      controls.dampingFactor = orbitConfig.dampingFactor;
+      controls.enableZoom = orbitConfig.enableZoom;
+    } else {
+      mainCamera = camera;
+      mainCamera.aspect = window.innerWidth / window.innerHeight;
+      mainCamera.updateProjectionMatrix();
+    }
+
+    //アニメーションセット
+    if (mixer) {
+      animationMixer = mixer;
+      animationMixer.stopAllAction();
+      for (let i = 0; i < animations.length; i++) {
+        let action = animationMixer.clipAction(animations[i]);
+        action.play();
+      }
+
+      // カメラの時間を取得
+      const cameraClip = animations.find((item) =>
+        item.name.match(/^Camera.*/g)
+      );
+      if (cameraClip) {
+        duration = cameraClip.duration;
+      }
+    }
+
+    animate();
+
+    console.log("init");
+  };
+
+  /**
+   * @description ブラウザのスクロール量 three.js側に渡す
+   * @param {float} move
+   */
+  const setMoveProgress = (move) => {
+    moveProgress = move;
+  };
+
+  /**
+   * @description レンダリング
+   */
+  const render = () => {
+    const t = moveProgress * duration;
+    // console.log(t, moveProgress, duration);
+    renderer.render(scene, mainCamera);
+    if (animationMixer) {
+      //ミキサー経過時間をセット
+      animationMixer.setTime(t);
+      //0秒再生
+      animationMixer.update(0);
+    }
+  };
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    render();
+  };
+
+  //GLTFの追加
+  const addGLTF = (url) => {
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load(url, function (data) {
+      const gltf = data;
+      const object = gltf.scene;
+      object.position.set(0, 0, 0);
+      object.rotation.set(rotationToRadian(0), 0, 0);
+      scene.add(object);
+    });
+  };
+
+  const loadGLTF = (url) => {
+    return new Promise((resolve) => {
+      const gltfLoader = new GLTFLoader();
+      gltfLoader.load(
+        url,
+        (gltf) => {
+          //モデルロード
+          const model = gltf.scene;
+          model.position.set(0, 0, 0);
+
+          let animations;
+          let camera;
+          let mixer;
+
+          //アニメーションがあればロード
+          if (gltf.animations) {
+            animations = gltf.animations;
+          }
+
+          //カメラあればロード
+          if (gltf.cameras[0]) {
+            // カメラ一つを想定
+            camera = gltf.cameras[0];
+          }
+
+          //複数ある場合はミキサー
+          if (animations && animations.length) {
+            mixer = new THREE.AnimationMixer(gltf.scene);
+          }
+
+          resolve({ model, animations, camera, mixer });
+        },
+        (error) => {
+          console.log("An error happened", error);
+        }
+      );
+    });
+  };
+
+  return {
+    //シーンを初期化
+    init,
+    //
+    addGLTF,
+    // スクロール量を設定
+    setMoveProgress,
+  };
+};
--- src/components/ThreeScene/indexAnimate.vue
+++ src/components/ThreeScene/indexAnimate.vue
@@ -12,99 +12,13 @@
   //import * as THREE from "three";
   // eslint-disable-next-line
   import * as THREE from "three/build/three.module";
-  import { Scene } from "./components/scene";
+  import { Scene } from "./components/scene_animate";
   import { STATE, EnterState } from "../../util/Events";
 
   let scene;
 
   //固定スクロールする範囲
   const fixLength = 10000;
-
-  // カメラの移動座標
-  const cameraPositions = [
-    // 入口
-    { x: 50, y: 20.0, z: 45 },
-    { x: 0, y: 1.0, z: 0 },
-    { x: -4, y: 1.2, z: 0 },
-
-    //電球
-    { x: -12, y: 1.2, z: 0 },
-
-    //風呂
-    { x: -16, y: 1.3, z: 0 },
-    { x: -18, y: 1.3, z: 0 },
-    { x: -20, y: 1.0, z: 0 },
-    { x: -22, y: 1.0, z: 0 },
-    // { x: -22, y: 1.3, z: 0 },
-
-    //木で支えられた通路
-    // { x: -22, y: 1.3, z: 1 },
-    { x: -23, y: 1.3, z: 0 },
-    { x: -24, y: 1.3, z: 0 },
-    { x: -30, y: 1.3, z: 0 },
-    { x: -35, y: 1.3, z: 0 },
-
-    // shaftB
-    { x: -50, y: 1.3, z: 0 },
-
-    //外へ
-    { x: -70, y: 1.3, z: 0 },
-    { x: -90, y: 1.3, z: 0 },
-
-    // 便所
-    { x: -100, y: 10, z: 20 },
-    { x: -115, y: 20, z: 30 },
-    { x: -120, y: 20, z: 30 },
-
-    //水
-    { x: -120, y: 20, z: 30 },
-    { x: -125, y: 15, z: 25 },
-    { x: -128, y: 5, z: 2 },
-    { x: -130, y: 4, z: 1 },
-  ];
-
-  // 注視点の移動座標
-  const targetPositons = [
-    // 入口
-    { x: -45, y: 0.3, z: 0 },
-    { x: 2, y: 0.3, z: 0 },
-    { x: 0, y: 0.3, z: 0 },
-    { x: 0, y: 0.3, z: 0 },
-    { x: -1, y: 0.3, z: 0 },
-    { x: -5, y: 0.3, z: 0 },
-
-    //電球
-    { x: -15, y: 0.3, z: 0 },
-
-    //風呂
-    { x: -18, y: 0.3, z: 0 },
-    { x: -19, y: 0.3, z: -1 },
-    { x: -20, y: 0.3, z: -2 },
-    { x: -21, y: 0.3, z: -5 },
-
-    //木で支えられた通路
-    { x: -22, y: 0.3, z: -2 },
-    { x: -23, y: 0.3, z: -1 },
-    { x: -25, y: 0.3, z: 0 },
-    { x: -40, y: 1.0, z: 0 },
-
-    // shaftB
-    { x: -60, y: 1.0, z: 0 },
-    { x: -82, y: 1.0, z: 0 },
-
-    // 外へ
-    { x: -100, y: 1.0, z: 0 },
-
-    // 便所
-    { x: -110, y: 2, z: 0 },
-    { x: -115, y: 3, z: 0 },
-    { x: -120, y: 3, z: 1 },
-    { x: -124, y: 4, z: 2 },
-    { x: -130, y: 5, z: 2 },
-
-    //水
-    { x: -130, y: 5, z: 2 },
-  ];
 
   // イベント
   export const callbackEvents = [
@@ -122,7 +36,7 @@
     // { id: 12, start: 0.86, length: 0.1 },
   ];
 
-  const modelUrl = "/data/data.glb";
+  const modelUrl = "/data/sampleAnim.glb";
 
   export default {
     name: "ThreeScene",
@@ -145,16 +59,11 @@
 
       scene = Scene();
       scene.init({
+        modelUrl: modelUrl,
         canvasId: "three",
         width,
         height,
-        cameraPosition: cameraPositions[0],
-        targetPosition: targetPositons[0],
       });
-      scene.animate();
-
-      //ここでオブジェクトをシーンに追加
-      this.addObject();
 
       //高さ固定
       const fixRange = callbackEvents
@@ -199,31 +108,6 @@
         });
       });
     },
-    methods: {
-      addObject() {
-        // scene.addObject({
-        //   radius: 5,
-        //   segments: 32,
-        //   position: new THREE.Vector3(0.0, 0.0, 0.0),
-        // });
-        // const loader = new GLTFLoader();
-        // const url = "/public/data/desk_v2.gltf";
-        // loader.load(url, (data) => {
-        //   const gltf = data;
-        //   const object = gltf.scene;
-        //   this.scene.add(object);
-        // });
-        scene.addGLTF(modelUrl);
-
-        //カメラ移動パス作成
-        scene.setCameraPositions(cameraPositions);
-
-        //注視点パス作成
-        scene.setTargetPositions(targetPositons);
-
-        // scene.setOrbitCont();
-      },
-    },
   };
 </script>
 <style scoped>
@@ -236,7 +120,7 @@
     height: 100%;
     /* border: 10px solid red; */
     box-sizing: border-box;
-    /* transform: scaleY(-1) scaleX(-1); */
+    transform: scaleY(-1) scaleX(-1);
   }
 
   .wrapper {
