a4f71b2618b06d258289e22bfe5b103471586d05
Konami Ota <konami.backham@gmail.com>
2022-01-25 13:16:53 +0900

    スタイル修正

--- src/pages/Contents.vue
+++ src/pages/Contents.vue
@@ -33,78 +33,80 @@
   </div>
 </template>
 <script>
-  import Slider, { SLIDER_TYPE } from "../components/Slides/index.vue";
+import Slider, { SLIDER_TYPE } from "../components/Slides/index.vue";
 
-  export default {
-    components: {
-      Slider,
+export default {
+  components: {
+    Slider,
+  },
+  props: {
+    items: {
+      type: Array,
     },
-    props: {
-      items: {
-        type: Array,
-      },
-    },
-    data: () => {
-      return {
-        SLIDER_TYPE,
-        SETTING: {
-          wheelControl: false,
-          // infiniteScroll: true,
-          // itemsToShow: 2.1,
-          breakpoints: {
-            600: {
-              itemsToShow: 4.1,
-            },
+  },
+  data: () => {
+    return {
+      SLIDER_TYPE,
+      SETTING: {
+        wheelControl: false,
+        // infiniteScroll: true,
+        // itemsToShow: 2.1,
+        breakpoints: {
+          600: {
+            itemsToShow: 4.1,
           },
         },
-      };
-    },
-  };
+      },
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
-  .contents {
-    padding-top: 10.25em;
-    background-image: url("~@/assets/image/background.png");
-  }
+.contents {
+  padding-top: 10.25em;
+  background-image: url("~@/assets/image/background.png");
+}
 
-  .content-section {
-    padding: 8em 0 0 0;
-  }
-  .content-wrapper {
-    padding: 2em 4em;
-  }
-  h2 {
-    margin: 0;
-    font-size: 2.5em;
-    font-weight: normal;
-    letter-spacing: 0.5em;
-  }
+.content-section {
+  padding: 8em 0 0 0;
+}
+.content-wrapper {
+  padding: 2em 4em;
+}
+h2 {
+  margin: 0;
+  font-size: 2.5em;
+  font-weight: normal;
+  letter-spacing: 0.5em;
+}
 
-  .content-subtitle {
-    font-size: 1.5em;
-  }
-  .content-text {
-    inline-size: 42em;
-    overflow-wrap: break-word;
-    margin-left: auto;
-    font-size: 1em;
-  }
+.content-subtitle {
+  font-size: 1.5em;
+}
+.content-text {
+  inline-size: 42em;
+  overflow-wrap: break-word;
+  margin-left: auto;
+  font-size: 1em;
+  line-height: 1.625em;
+  letter-spacing: 0.1875em;
+}
 
-  /**
+/**
   * Slider
   */
 
-  .slider-wrapper {
-    display: flex;
-    justify-content: flex-end;
-  }
-  .slider {
-    padding: 16px 0 0 16px;
-    margin-top: 5em;
-    width: 90%;
-    background: rgba(0, 0, 0, 0.1);
-  }
+.slider-wrapper {
+  display: flex;
+  justify-content: flex-end;
+}
+.slider {
+  padding: 16px 0 0 16px;
+  margin-top: 5em;
+  width: 90%;
+  background: rgba(0, 0, 0, 0.1);
+}
 
-  @media screen and (max-width: 559px) {
-  }
+@media screen and (max-width: 559px) {
+}
 </style>
